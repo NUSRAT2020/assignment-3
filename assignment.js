9958a5f433c6150703c9dcff44b70562d01736de
NUSRAT2020 <ipaazom@gmail.coom>
2021-02-20 23:05:33 -0500

    first part

---
+++ assignment.js
@@ -0,0 +1,9 @@
+var kilometer = 5;
+
+  function kilometerToMeter(kilometer) {
+
+        var meter = kilometer * 1000 ;
+        return meter ;      
+  }
+  var result = kilometerToMeter(7)
+ console.log(result);
